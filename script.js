af6a0d0e5f6cdd275e62959325a45fb383c2c8a1
DanielHesslow <daniel.hesslow@gmail.com>
2019-12-03 21:40:43 +0100

    blah

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
+var offer_to_respond =  document.getElementById("offer to respond");
 var button = document.getElementById("button");
+var button_acc = document.getElementById("button_acc");
+var button_rej = document.getElementById("button_rej");
+
+
+var make_offer = document.getElementById("make offer");
+var waiting = document.getElementById("waiting");
+var respond_offer = document.getElementById("respond offer");
+waiting.style.display = "none"
+var index = -1
+var round = 1;
+var offer = -404;
+
+function show(layout){
+    waiting.style.display = "none"
+    make_offer.style.display = "none"
+    respond_offer.style.display = "none"
+
+    if(layout == "waiting"){
+        waiting.style.display = "block"
+    }
+    if(layout == "make offer"){
+        make_offer.style.display = "block"
+    }
+    if(layout == "respond offer"){
+        respond_offer.style.display = "block"
+    }
+}
+
+
 var ID = undefined;
+var users = undefined;
+var offers = undefined;
+
+show("make offer")
 
 function display_text(slider_value){
-    return 'Keep ' + slider_value + "% - Give " + (100 - slider_value) + "%";
+    return 'You get ' + slider_value + "% - They get " + (100 - slider_value) + "%";
 }
 
 output.innerHTML = display_text(slider.value); // Display the default slider value
@@ -20,15 +54,35 @@ button.onclick = function() {
     publishOffer(slider.value, ID);
 }
 
+
+function send_offer_response(value){
+    offer.v.status = value;
+    firebase.database().ref('/offers/' + offer.k).set(offer.v);
+    show("waiting")
+}
+
+
+button_acc.onclick = function() {
+    send_offer_response("accepted")
+}
+
+button_rej.onclick = function() {
+    send_offer_response("rejected")
+}
+
+
 function publishOffer(offer, id) {
     firebase.database().ref('offers/'+id).set({
-        offer
+        offer: offer,
+        status: "no-reponse"
     })
     console.log('sent offer:' + offer);
+    show("respond offer")
+    get_offer()
 }
 
 function make_id(){
-    firebase.database().ref('users/').push({
+    return firebase.database().ref('users/').push({
         user:'IAMUSER'
     }).then(function(x){
         console.log(x.key)
@@ -38,8 +92,24 @@ function make_id(){
 
 make_id()
 
-function get_offers() {
-    firebase.database().ref('/offers/-1').once('value').then(function(snapshot) {
+function get_offer() {
+    firebase.database().ref('/offers/').once('value').then(function(snapshot) {
         console.log(snapshot.val());   
+        offers = snapshot.val()
+        index = Object.keys(offers).indexOf(ID)
+        keys = Object.keys(offers);
+        values  = Object.values(offers);
+        let offer_index = (index + round - 2 + keys.length) % keys.length 
+        offer = {k: keys[offer_index], v: values[offer_index]}
+        offer_to_respond.innerHTML = display_text(100-offer.v.offer)
     })
 }
+
+function get_users(){
+    firebase.database().ref('users/').once('value').then(function(x){
+        console.log(x.val())
+        users = x.val()
+        index = Object.keys(users).indexOf(ID)
+    });
+}
+
